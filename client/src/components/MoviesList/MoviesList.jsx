78425bd88c55de741cb7a130f536fa96ceed1785
bunnyohare <739120+bunnyohare@users.noreply.github.com>
2024-04-23 00:16:11 -0400

    Fix styling of Write A Review button.

--- client/src/components/MoviesList/MoviesList.jsx
+++ client/src/components/MoviesList/MoviesList.jsx
@@ -17,22 +17,28 @@ function MoviesList({ movies, openAddReviewForm }) {
       <h3 id="Movie-List-Heading">Found Movies List</h3>
       {/* Conditionally render the movieList based on showFavorites */}
       {movies.map((movie) => (
-        <div key={movie.imdbID}>
+        <div class="movielist-item" key={movie.imdbID}>
           <h3>{movie.Title}</h3>
           <p>Year: {movie.Year}</p>
           <p>IMDb ID: {movie.imdbID}</p>
-          <img
-            src={movie.Poster}
-            onError={(e) => {
-              e.target.onerror = null;
-              e.target.src =
-                "https://raw.githubusercontent.com/bunnyohare/SBA-320H/main/images/placeholder-omdb.jpg";
-            }}
-            alt={movie.Title}
-          />
-          <button onClick={() => handleReviewClick(movie)}>
-            Write A Review
-          </button>
+          <div class="movie-details">
+          <div id="div-img">
+            <img
+              src={movie.Poster}
+              onError={(e) => {
+                e.target.onerror = null;
+                e.target.src =
+                  "https://raw.githubusercontent.com/bunnyohare/SBA-320H/main/images/placeholder-omdb.jpg";
+              }}
+              alt={movie.Title}
+            />
+          </div>
+          <div id="div-button">
+            <button id="write-button" onClick={() => handleReviewClick(movie)}>
+              Write A Review
+            </button>
+          </div>
+          </div>
         </div>
       ))}
     </div>
